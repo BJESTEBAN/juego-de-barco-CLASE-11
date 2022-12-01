1039dd3c6406b4387a8655124dbe1cc4a1632fe1
BJESTEBAN <118493195+BJESTEBAN@users.noreply.github.com>
2022-12-01 12:37:42 -0500

    Add files via upload

---
+++ juego del barco CLASE-11.js
@@ -0,0 +1,18 @@
+function preload(){
+seaImg = loadImage("sea.pag");
+shipImg1 =loadAnimation (//enlista los nombres de los png que desees);
+}
+
+function setup(){
+spriteName.addImage(seaImg);
+createCanvas(400,400);
+}
+
+functioncdraw() {
+//codigo parareiniciar el fondo.
+if (sea.x <0) {
+    sea.x=sea.wisth/2;
+}
+blackground("blue");
+
+}
